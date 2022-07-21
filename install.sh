1ce214ad6dc8e73327f2a39f6b9cb90b0f38c4a8
Loyzi <107613868+Loyzi@users.noreply.github.com>
2022-07-21 10:21:19 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@ apt update -y
 apt upgrade -y
 apt install nano htop neofetch speedtest-cli golang -y
 go get github.com/yudai/gotty
-cd ~ ; ~/go/bin/gotty -a 0.0.0.0 -p 8080 -w -c root:123123 bash
 BLUE='\033[1;34m'
 RED='\033[1;31m'
 echo -e "${RED}Don't forget to open the port in GoormIDE settings !"
