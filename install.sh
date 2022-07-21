170ae7817abb0c2b4ad46692aac1d4b5afe14229
Loyzi <107613868+Loyzi@users.noreply.github.com>
2022-07-21 10:20:01 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,4 @@ BLUE='\033[1;34m'
 RED='\033[1;31m'
 echo -e "${RED}Don't forget to open the port in GoormIDE settings !"
 echo -e "${BLUE}Command to start GoTTY: cd ~ ; ~/go/bin/gotty -a 0.0.0.0 -p INTERNAL_PORT -w -c LOGIN:PASSWORD bash"
+rm install.sh
