55088be6215fb9f9b4e51eb649115359d846a33a
Loyzi <107613868+Loyzi@users.noreply.github.com>
2022-07-21 23:28:56 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 apt update -y
 apt upgrade -y
-apt install nano htop neofetch speedtest-cli golang -y
+apt install nano htop tmux golang -y
 go get github.com/yudai/gotty
 BLUE='\033[1;34m'
 RED='\033[1;31m'
